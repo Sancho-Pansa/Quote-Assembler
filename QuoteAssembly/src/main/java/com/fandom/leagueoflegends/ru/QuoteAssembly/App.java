48e1fc31da6dd8af343897831ade08cbc1feda00
SanchoPanzsa <zelgadis4@gmail.com>
2020-01-13 21:59:17 +0300

    First commit

---
+++ QuoteAssembly/src/main/java/com/fandom/leagueoflegends/ru/QuoteAssembly/App.java
@@ -0,0 +1,13 @@
+package com.fandom.leagueoflegends.ru.QuoteAssembly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
